f7de31b101f784e0d1ad9efc18f876160d1c6c65
avahey <andrewwvahey@gmail.com>
2015-04-14 22:41:25 -0500

    add method to property for generation of full address

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,8 +1,12 @@
 class Property < ActiveRecord::Base
   validates_presence_of :address
   validates_uniqueness_of :address
-  geocoded_by :address 
+  geocoded_by :full_address
   after_validation :geocode
+
+  def full_address
+    "#{self.address} #{self.city} #{self.state} #{self.zip}"
+  end
   
   def self.find_all_by_query(query)
     query = query.downcase
