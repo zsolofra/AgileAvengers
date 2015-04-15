34bb5e0607c51ee08489e0fb0b2a0324b10552cc
Brian Dawson and Brian Poppe <dawson.brian92@gmail.com>
2015-04-15 03:54:02 +0000

    Add reviews linked to properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,7 +2,7 @@ class Property < ActiveRecord::Base
   validates_presence_of :address
   validates_uniqueness_of :address
   
-  has_many :reviews
+  has_many :reviews, :dependent => :destroy
   
   def self.find_all_by_query(query)
     query = query.downcase
