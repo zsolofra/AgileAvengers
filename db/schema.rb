60ff8f59f2f3564ca2da72d2ed0f2bc4d43eaec8
avahey <andrewwvahey@gmail.com>
2015-04-14 21:36:34 -0500

    add fields to schema on migration rake?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411203530) do
+ActiveRecord::Schema.define(version: 20150415002150) do
 
   create_table "properties", force: :cascade do |t|
     t.string   "address"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20150411203530) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
