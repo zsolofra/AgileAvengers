ba02ed7a547f1428893198a6e21681ab6713941a
avahey <andrewwvahey@gmail.com>
2015-04-17 22:56:43 -0500

    add uid field to table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418022034) do
+ActiveRecord::Schema.define(version: 20150418034136) do
 
   create_table "properties", force: :cascade do |t|
     t.string   "address"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20150418022034) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
