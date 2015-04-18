21a75404b06758103c5612111835ee1067d0c45f
avahey <andrewwvahey@gmail.com>
2015-04-17 22:54:25 -0500

    add facebook id and secret

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,4 +256,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :facebook, "1576448599304587", "3ca207d6e7995e03f498701aafb018fa"
 end
