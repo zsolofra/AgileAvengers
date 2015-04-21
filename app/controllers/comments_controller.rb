5a195a7f10fbcb16bbb630ffdd69bfaba042f143
Brian Dawson <dawson.brian92@gmail.com>
2015-04-21 23:10:38 +0000

    Add upvote and downvote to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -57,6 +57,18 @@ class CommentsController < ApplicationController
     end
   end
   
+  def upvote
+    @comment = Comment.find(params[:id])
+    @comment.upvote_by current_user
+    redirect_to :back
+  end
+  
+  def downvote
+    @comment = Comment.find(params[:id])
+    @comment.downvote_by current_user
+    redirect_to :back
+  end
+  
   
   
   private
