1d8669e2beaeb08ba2423da6cab7f9098d2a6133
bpoppe <poppe_10_2@yahoo.com>
2015-04-21 03:21:02 -0500

    Defined and added CRUD methods.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :set_property
+  before_action :set_review
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  
   
   def index
     @comment = @review.comments.all
+   # @comment = Comment.all
     
-    respond_to do |format|
-      format.html
-      format.xml {render :xml => @comment}
-    end
   end
   
   def show
@@ -19,20 +20,17 @@ class CommentsController < ApplicationController
   end
   
   def new
-    @comment = @review.comments.build
-    
-    respond_to do |format|
-      format.html
-      format.xml {render :xml => @comment}
-    end
+    @comment = @review.comments.new
   end
   
   def create
+    
+    
       @comment = @review.comments.build(comment_params)
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to([@comment.review, @comment], :notice => 'Comment was successfully created.') }
+        format.html { redirect_to([@property, @review, @comment], :notice => 'Comment was successfully created.') }
         format.xml { render :xml => @comment, :status => :created, :location => [@comment.review, @comment]}
       else
         format.html { render :action => "new" }
@@ -41,12 +39,16 @@ class CommentsController < ApplicationController
     end
   end
   
+  def edit 
+    #@comment = @review.comments.find(params[:id])
+  end
+  
   def update
-    @comment = @review.comments.find(params[:id])
-    
+   @comment = @review.comments.find(params[:id])
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to [@comment.review, @comment], notice: 'Comment was successfully updated.' }
+        format.html { 
+        redirect_to([@property, @review, @comment], :notice => 'Comment was successfully updated.') }
         format.xml { head :ok }
       else
         format.html { render :action => "edit" }
@@ -55,29 +57,24 @@ class CommentsController < ApplicationController
     end
   end
   
-  def destroy
-    @comment = @review.comments.find(params[:id])
-    
-    @comment.destroy
-    
-    respond_to do |format|
-      format.html { redirect_to(review_comments_url) }
-      format.xml { head :ok }
-    end
-  end
+  
   
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_property
+      @property = Property.find(params[:property_id])
+    end
+    
+    def set_review
+      @review = @property.reviews.find(params[:review_id])
+    end
+  
     def set_comment
       @comment = @review.comments.find(params[:id])
     end
   
-    def set_review
-      @review = Review.find(params[:review_id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:review_id, :likes, :dislikes, :user_id, :comment)
+      params.require(:comment).permit(:review_id, :likes, :dislikes, :comment, :user_id)
     end
 end
