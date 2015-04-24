93bdb1e57673abb0288c025d24f5dcd00703dc8f
Brian Dawson <dawson.brian92@gmail.com>
2015-04-24 21:31:44 +0000

    Change 30 minute method name

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,4 +7,8 @@ class Review < ActiveRecord::Base
   def score
     return self.get_upvotes.size - self.get_downvotes.size
   end
+  
+  def has_time_passed?
+    created_at < 30.minutes.ago
+  end
 end
