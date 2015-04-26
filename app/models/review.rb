7c31dbb687bade734eee3a5870845a68abdcce2a
Jessica Groat <jessicalgroat@gmail.com>
2015-04-26 18:53:48 +0000

    Reviews can no longer be submitted when left blank

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ActiveRecord::Base
   acts_as_votable
   belongs_to :property
   has_many :comments, :dependent => :destroy
+  validates :review, :presence => true
   
   def score
     return self.get_upvotes.size - self.get_downvotes.size
