d1be172745993e11b1125b557f3fbc1c8397d1a7
Brian Dawson <dawson.brian92@gmail.com>
2015-04-26 20:13:32 +0000

    Add rater to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
   
+  ratyrate_rater
+  
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.provider = auth.provider
