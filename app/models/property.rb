af125ef4a09482457417ec97712f8354d9e12212
Brian Dawson <dawson.brian92@gmail.com>
2015-04-26 20:12:45 +0000

    Add property as ratable

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,6 +4,7 @@ class Property < ActiveRecord::Base
   validates_uniqueness_of :address
   geocoded_by :full_address
   after_validation :geocode
+  ratyrate_rateable 'overall_rating'
 
   def full_address
     "#{self.address} #{self.city} #{self.state} #{self.zip}"
