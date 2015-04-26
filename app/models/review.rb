f0a76da9162b16017b165ee6ea19f7336f66d146
Brian Dawson <dawson.brian92@gmail.com>
2015-04-26 21:56:29 +0000

    Add validation that does not allow users to make more than 1 review per property

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ActiveRecord::Base
   acts_as_votable
   belongs_to :property
   has_many :comments, :dependent => :destroy
+  validates :user_id, :uniqueness => { :scope => :property_id,
+    :message => "may only write one review per property." }
   
   def score
     return self.get_upvotes.size - self.get_downvotes.size
