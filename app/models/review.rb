6a237b1e4a5b6a192ab7c98438969a00db292ebc
bpoppe <poppe_10_2@yahoo.com>
2015-04-28 09:16:03 -0500

    No Change

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,9 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :property
   has_many :comments, :dependent => :destroy
   validates :user_id, :uniqueness => { :scope => :property_id,
-    :message => "may only write one review per property." }
-  
-  ratyrate_rateable 'property_rating'
+   :message => "may only write one review per property." }
   
   def score
     return self.get_upvotes.size - self.get_downvotes.size
