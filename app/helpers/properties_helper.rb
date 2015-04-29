c3740eafb665790eb1dd76d0d5c1152365750650
Brian Dawson <dawson.brian92@gmail.com>
2015-04-29 01:23:11 +0000

    Add show utilities method

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,10 @@
 module PropertiesHelper
+  
+  def display_utilities_for(property)
+    if property.utilities.match(/^(\d)+$/)
+      number_to_currency(property.utilities)
+    else
+       property.utilities
+    end
+  end
 end
